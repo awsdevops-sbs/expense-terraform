8d34f5ef3eb5e1ca0b65f2f3eedf7d02544edb23
akram <akram.khan200@gmail.com>
2026-01-06 14:21:02 +0530

    terraform example 10 function

---
+++ run.sh
@@ -0,0 +1,18 @@
+env=$1
+action=$2
+
+if [ -z "$env" ]; then
+echo "input  dev\prod\stage\prod  is missing"
+exit  1
+fi
+
+if [ -z "$action" ]; then
+echo "input (plan/apply/init) is missing"
+exit  1
+fi
+
+rm -rf ./terraform
+
+terraform init -backend-config=env-$env/state.tfvars
+terraform $action -var-file=env-$env/main.tfvars -auto-approve
+
