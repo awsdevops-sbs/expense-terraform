774c37b090227804397ff1d9c320d079e91f1b80
akram <akram.khan200@gmail.com>
2026-01-07 22:46:15 +0530

    creating piplines for terraform new changes

--- main.tf
+++ main.tf
@@ -8,14 +8,21 @@ module "frontend" {
   zone_id = var.zone_id
 }
 
-# module "backend" {
-#   source = "./modules/app"
-#   name = "frontend"
-#   instance_type = "t3.small"
-# }
-#
-# module "mysql" {
-#   source = "./modules/app"
-#   name = "frontend"
-#   instance_type = "t3.small"
-# }
+module "backend" {
+  source = "./modules/app"
+  component = "backend"
+  instance_type = var.instance_type
+  env = var.env
+  ssh_user = var.ssh_user
+  ssh_pass = var.ssh_pass
+  zone_id = var.zone_id
+}
+module "mysql" {
+  source = "./modules/app"
+  component = "mysql"
+  instance_type = var.instance_type
+  env = var.env
+  ssh_user = var.ssh_user
+  ssh_pass = var.ssh_pass
+  zone_id = var.zone_id
+}
