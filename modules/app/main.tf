7a7c6152f146697850a38a597ee3e7e94a111fa5
akram <akram.khan200@gmail.com>
2026-01-20 17:11:09 +0530

    install Vault

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -29,7 +29,7 @@ resource "null_resource" "ansible" {
     inline = [
       "sudo pip3.11 install ansible",
       "ansible-pull  -i localhost, -U https://github.com/awsdevops-sbs/ansible.git  get-secrets.yml -e role_name=${var.component}  -e env=${var.env} -e vault_token=${vault_token} ",
-      "ansible-pull  -i localhost, -U https://github.com/awsdevops-sbs/ansible.git  expense.yml -e role_name=${var.component}  -e env=${var.env} -e vault_token=${vault_token} -e '@secret.json'"
+      "ansible-pull  -i localhost, -U https://github.com/awsdevops-sbs/ansible.git  expense.yml -e role_name=${var.component}  -e env=${var.env} -e vault_token=${vault_token} -e '~@secret.json'"
 
     ]
   }
