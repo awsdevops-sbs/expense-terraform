914dae4df15098da0121b8f62579361ecbd3f75c
akram <akram.khan200@gmail.com>
2026-01-22 17:10:20 +0530

    install Vault

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -35,7 +35,7 @@ resource "null_resource" "ansible" {
       #"sudo /usr/bin/python3.13 -m pip install --upgrade pip",
       #"sudo /usr/bin/python3.13 -m pip install ansible hvac",
       "ansible-pull  -i localhost, -U https://github.com/awsdevops-sbs/ansible.git  get-secrets.yml -e role_name=${var.component}  -e env=${var.env} -e vault_token=${var.vault_token} ",
-      "ansible-pull  -i localhost, -U https://github.com/awsdevops-sbs/ansible.git  expense.yml -e role_name=${var.component}  -e env=${var.env} -e vault_token=${var.vault_token} -e  @~/secrets.json -e @~/@app.json"
+      "ansible-pull  -i localhost, -U https://github.com/awsdevops-sbs/ansible.git  expense.yml -e role_name=${var.component}  -e env=${var.env} -e vault_token=${var.vault_token} -e  @~/secrets.json -e @~/app.json"
 
     ]
   }
