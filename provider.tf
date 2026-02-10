16d01bf03562126a7d6150c5635336bc8712cc8b
akram <akram.khan200@gmail.com>
2026-02-10 17:36:23 +0530

    install Vault 1.1 43544531

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 provider "http" {}
 
 provider "vault" {
-  address             = "https://vault.awsdevops.sbs:8200"
+  address             = "https://vault.awsdevops16297.sbs:8200"
   token               = var.vault_token
   skip_tls_verify     = true
 }
