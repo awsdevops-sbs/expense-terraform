06b95b40bc4d491ea63c6e7c6120f4bc1739eb75
akram <akram.khan200@gmail.com>
2026-02-17 17:09:37 +0530

    logstash 1.1

--- main.tf
+++ main.tf
@@ -1,32 +1,32 @@
-module "frontend" {
-  depends_on = [module.backend]
-  source        = "./modules/app"
-  component     = "frontend"
-  instance_type = var.instance_type
-  vault_token   = var.vault_token
-  env           = var.env
-  zone_id = var.zone_id
-}
-
-module "backend" {
-  depends_on = [module.mysql]
-  source = "./modules/app"
-  component = "backend"
-  instance_type = var.instance_type
-  vault_token   = var.vault_token
-  env = var.env
-
-
-  zone_id = var.zone_id
-}
-module "mysql" {
-  source = "./modules/app"
-  component = "mysql"
-  instance_type = var.instance_type
-  env = var.env
-  vault_token   = var.vault_token
-  zone_id = var.zone_id
-}
+# module "frontend" {
+#   depends_on = [module.backend]
+#   source        = "./modules/app"
+#   component     = "frontend"
+#   instance_type = var.instance_type
+#   vault_token   = var.vault_token
+#   env           = var.env
+#   zone_id = var.zone_id
+# }
+#
+# module "backend" {
+#   depends_on = [module.mysql]
+#   source = "./modules/app"
+#   component = "backend"
+#   instance_type = var.instance_type
+#   vault_token   = var.vault_token
+#   env = var.env
+#
+#
+#   zone_id = var.zone_id
+# }
+# module "mysql" {
+#   source = "./modules/app"
+#   component = "mysql"
+#   instance_type = var.instance_type
+#   env = var.env
+#   vault_token   = var.vault_token
+#   zone_id = var.zone_id
+# }
 
 module "vpc" {
   source = "./modules/vpc"
