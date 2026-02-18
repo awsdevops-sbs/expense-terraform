6507a0932100bcaad4ce07421686be61d03bda80
akram <akram.khan200@gmail.com>
2026-02-18 16:57:26 +0530

    subnets

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -30,6 +30,8 @@ resource "aws_instance" "instance" {
   ami = data.aws_ami.ami.image_id
   instance_type = var.instance_type
 
+  subnet_id = var.subnets[0]
+
 # vpc_security_group_ids = [data.aws_security_group.selected.id]
 
   vpc_security_group_ids = aws_security_group.main.id
