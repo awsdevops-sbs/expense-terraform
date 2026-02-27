47a3a900bc154dc9e178efa17b44594672972f4c
akram <akram.khan200@gmail.com>
2026-02-27 19:33:23 +0530

    load balacer

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -121,7 +121,7 @@ resource "aws_lb_target_group" "main" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 
-  deregistration_delay = "15"
+  deregistration_delay = 15
 
   health_check {
     path                = "/health"
