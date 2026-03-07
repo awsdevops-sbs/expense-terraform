e83502b0a210d11514bde04fae856043e736fff4
akram <akram.khan200@gmail.com>
2026-03-07 21:02:57 +0530

    load balacer

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -126,7 +126,7 @@ resource "aws_route_table" "frontend" {
   vpc_id      = aws_vpc.main.id
 
   route {
-    cidr_block = var.default_vpc_id
+    cidr_block = var.default_cidr_block
     vpc_peering_connection_id = aws_vpc_peering_connection.main.id
   }
 
