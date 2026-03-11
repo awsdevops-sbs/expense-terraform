be93f5a79b6fb9c5715164f022e03feabaf068eb
akram <akram.khan200@gmail.com>
2026-03-11 21:44:06 +0530

    rds

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -4,14 +4,14 @@ variable "family" {}
 variable "subnets" {}
 variable "storage_type" {
 
-  default = null
+
 }
 variable "skip_final_snapshot" {}
 variable "instance_class" {}
 variable "engine_version" {}
 variable "engine" {}
 variable "allocated_storage" {
-  default = null
+
 }
 
 variable "vpc_id" {}
