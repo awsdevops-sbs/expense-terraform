021bd19f4e3cd651d204f6da4ba14f3e3e282c92
akram <akram.khan200@gmail.com>
2026-03-15 01:00:45 +0530

    ASG 1.1

--- modules/app-asg/main.tf
+++ modules/app-asg/main.tf
@@ -61,8 +61,10 @@ resource "aws_launch_template" "main" {
 resource "aws_autoscaling_group" "main" {
   name                = "${var.component}-${var.env}"
   desired_capacity    = var.min_capacity
-  min_capacity            = var.min_capacity
-  max_capacity        = var.max_capacity
+  min_size           = var.min_capacity
+
+
+  max_size      = var.max_capacity
   vpc_zone_identifier = var.subnets
   target_group_arns =  [aws_lb_target_group.main.arn]
 
