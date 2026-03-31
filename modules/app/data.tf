7202cd5fcec95d8b9f837c2cf0f1cce2239fd57f
akram <akram.khan200@gmail.com>
2026-03-31 19:53:04 +0530

    ASG 1.1

--- modules/app/data.tf
+++ modules/app/data.tf
@@ -1,14 +1,24 @@
+# data "aws_ami" "ami" {
+#   #most_recent      = true
+#   #name_regex       = "RHEL-9-DevOps-Practice"
+#    owners           = ["973714476881"]
+#
+#   filter {
+#     name   = "image-id"
+#     values = ["ami-0220d79f3f480ecf5"]
+#   }
+#
+#
+# }
+
+
 data "aws_ami" "ami" {
-  #most_recent      = true
-  #name_regex       = "RHEL-9-DevOps-Practice"
-   owners           = ["973714476881"]
+  owners = ["973714476881"]
 
   filter {
     name   = "image-id"
     values = ["ami-0220d79f3f480ecf5"]
   }
-
-
 }
 
 # data "aws_security_group" "selected" {
